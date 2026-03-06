f00051a4af065f3846db2781f1a234ca15d5b7cc
Francisco Santos <xikaovs@gmail.com>
2026-03-05 21:02:37 -0300

    Add PageData struct and extractPageData function with tests

---
+++ page_data.go
@@ -0,0 +1,48 @@
+package main
+
+import "net/url"
+
+type PageData struct {
+	URL            string
+	Heading        string
+	FirstParagraph string
+	OutgoingLinks  []string
+	ImageURLs      []string
+}
+
+func extractPageData(html, pageURL string) (PageData, error) {
+	pageData := PageData{
+		URL: pageURL,
+	}
+
+	heading, err := getHeadingFromHTML(html)
+	if err != nil {
+		return PageData{}, err
+	}
+	pageData.Heading = heading
+
+	firstParagraph, err := getFirstParagraphFromHTML(html)
+	if err != nil {
+		return PageData{}, err
+	}
+	pageData.FirstParagraph = firstParagraph
+
+	baseUrl, err := url.Parse(pageURL)
+	if err != nil {
+		return PageData{}, err
+	}
+
+	outgoingLinks, err := getURLsFromHTML(html, baseUrl)
+	if err != nil {
+		return PageData{}, err
+	}
+	pageData.OutgoingLinks = outgoingLinks
+
+	imageURLs, err := getImagesFromHTML(html, baseUrl)
+	if err != nil {
+		return PageData{}, err
+	}
+	pageData.ImageURLs = imageURLs
+
+	return pageData, nil
+}
