4c43fc7f1ba11f90cc1df64347d1ae073b4e5014
Francisco Santos <xikaovs@gmail.com>
2026-03-08 16:01:56 -0300

    Implement HTML fetching functionality and update main function for URL input handling

--- extract.go
+++ extract.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"net/http"
 	"net/url"
 	"strings"
 
@@ -97,4 +100,36 @@ func getImagesFromHTML(htmlBody string, baseURL *url.URL) ([]string, error) {
 	})
 
 	return urls, nil
+}
+
+func getHTML(rawURL string) (string, error) {
+	req, err := http.NewRequest("GET", rawURL, nil)
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Set("User-Agent", "BootCrawler/1.0")
+	client := http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode >= 400 {
+		return "", fmt.Errorf("Invalid status code: %v", res.StatusCode)
+	}
+
+	content_type := res.Header.Get("Content-Type")
+	if content_type != "text/html" {
+		return "", fmt.Errorf("Invalid content type: %v", content_type)
+	}
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(data), nil
 }
--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/url"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	} else if len(args) > 1 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	baseUrl, err := url.Parse(args[0])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("starting crawl of: %v\n", baseUrl)
+
+	data, err := getHTML(baseUrl.String())
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(data)
 }
